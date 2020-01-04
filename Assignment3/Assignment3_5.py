2b702426b3d9f82fe29a2f6bc42edcd23d01afd4
KAUSHAL ASHOK ZINE <32410264+kaushalzine@users.noreply.github.com>
2020-01-04 13:14:36 +0530

    Add files via upload

---
+++ Assignment3/Assignment3_5.py
@@ -0,0 +1,36 @@
+# 5.Write a program which accept N numbers from user and store it into List. Return addition of all
+# prime numbers from that List. Main python file accepts N numbers from user and pass each
+# number to ChkPrime() function which is part of our user defined module named as
+# MarvellousNum. Name of the function from main python file should be ListPrime().
+# Input : Number of elements : 11
+# Input Elements : 13 5 45 7 4 56 10 34 2 5 8
+# Output : 54 (13 + 5 + 7 +2 + 5) 
+
+
+
+from PrimeNumimport *;
+
+no=int(input(" Enter Number -:"));
+
+ListPrime(no)
+
+
+# Output-:
+# zine@DESKTOP-BU1PSC0 MINGW64 ~/Desktop/python/Assignment/Assignment3
+# $ python Assignment3_5.py
+#  Enter Number -:5
+# Enter element 1
+# 3
+# Enter element 2
+# 4
+# Enter element 3
+# 5
+# Enter element 4
+# 6
+# Enter element 5
+# 7
+# [3, 4, 5, 6, 7]
+
+
+# prime  num = [3, 5, 7]
+# 15
